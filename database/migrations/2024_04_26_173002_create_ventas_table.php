529987ba38118ae22727c30c86abe9bde8ec189f
Fabian Marriaga <fabiancamilo15@msn.com>
2024-04-26 12:33:29 -0500

    Migraciones ( Vehiculos, Clientes, Ventas)

---
+++ database/migrations/2024_04_26_173002_create_ventas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ventas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('vehiculo_id');
+            $table->unsignedBigInteger('cliente_id');
+            $table->foreign('vehiculo_id')->references('id')->on('vehiculos')->onDelete('cascade');
+            $table->foreign('cliente_id')->references('id')->on('clientes')->onDelete('cascade');
+            $table->date('fecha_venta');
+            $table->decimal('precio_final', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ventas');
+    }
+};
